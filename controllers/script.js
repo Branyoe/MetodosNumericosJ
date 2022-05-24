9a3993d100829896bd148dc5bcfd1c1fa854dd3b
branyoe <bhernandez1@ucol.mx>
2022-05-24 11:55:37 -0500

    feat: add getIntergralFormatted method

--- controllers/script.js
+++ controllers/script.js
@@ -1,135 +1,35 @@
 function calcular({ x0, x1, fx }) {
-  let integral = getFormattedFx(fx);
-  return integral 
+  let fxFormatted = getFormattedFx(fx);
+  let integralFormatted = getIntegralFormatted(fxFormatted)
+  return integralFormatted;
 }
 
 function getFormattedFx(fx = ""){
-  let integral = fx.trim();
-  integral = integral.split(" ")
-  integral = integral.map(v => {
-    return v.replace("^", "**");
-  });
-  return integral;
+  let fxFormatted = fx.trim();
+  fxFormatted = fxFormatted.split(" ")
+  return fxFormatted;
 }
 
-function bucle(xL, xU, Ea) {
-  let iteraciones = [];
-  let i = 0;
-  const datosIniciales = {
-    xL: xL,
-    xU: xU,
-    fxL: calcFx(xL),
-    fxU: calcFx(xU)
-  }
-
-  let xR = biseccion(datosIniciales.xL, datosIniciales.xU);
-  let fxR = calcFx(xR)
-  let dataRes = {
-    xR: xR,
-    fxR: fxR,
-    ...datosIniciales
-  }
-
-  iteraciones.push(dataRes);
-
-  let orden = calcOrden(dataRes);
-  let err = 0;
-  do {
-    i++;
-    try {
-      var { xL, fxL } = orden[0]
-      var { xU, fxU } = orden[1]
-    } catch (error) {
-      return { error: "La función no se encuentra en ese intervalo"}
+function getIntegralFormatted(fx = []){
+  let integralFormatted = fx.map(v => {
+    if(!v.includes("x")){
+      return `${v}*x`
     }
-    xR = biseccion(xL, xU);
-    fxR = calcFx(xR);
-    err = calcErr(xR, iteraciones[i - 1].xR);
-    dataRes = {
-      xR: xR,
-      fxR: fxR,
-      xL: xL,
-      fxL: fxL,
-      xU: xU,
-      fxU: fxU,
-      error: err
+    if(v.includes("^")){
+      let t1Fin, t1, str;
+      t1Fin = v.indexOf("x");
+      t1 = v.slice(0,t1Fin);
+      let exponente = parseFloat(v.slice(v.indexOf("^") + 1));
+      return str = `${t1}*((${"x^" + (exponente + 1)})/${exponente + 1})`;
+    }else{
+      let t1Fin, t1, str;
+      t1Fin = v.indexOf("x");
+      t1 = v.slice(0,t1Fin);
+      return str = `${t1}*((${"x^" + 2})/${2})`;
     }
-
-    orden = calcOrden(dataRes);
-    iteraciones.push(dataRes);
-  } while (dataRes.error > Ea);
-  return iteraciones;
-}
-
-function biseccion(x0, x1) {
-  return ((x0 + x1) / 2);
-}
-
-function calcFx(x) {
-  return (Math.pow(x, 3) - x - 1);
-}
-
-function calcErr(xNew, xOld) {
-  let res = ((xNew - xOld) / (xNew)) * 100
-  if (res < 0) {
-    return res * -1
-  }
-  return res
-}
-
-function calcOrden(data) {
-  let { xL, fxL, xR, fxR, xU, fxU } = data;
-  if (fxR < 0) {
-    if (fxU > 0) {
-      return [
-        {
-          xL: xR,
-          fxL: fxR
-        },
-        {
-          xU: xU,
-          fxU: fxU
-        }
-      ]
-    }
-    if (fxL > 0) {
-      return [
-        {
-          xL: xR,
-          fxL: fxR
-        },
-        {
-          xU: xL,
-          fxU: fxL
-        }
-      ]
-    }
-  } else {
-    if (fxU < 0) {
-      return [
-        {
-          xL: xU,
-          fxL: fxU
-        },
-        {
-          xU: xR,
-          fxU: fxR
-        }
-      ]
-    }
-    if (fxL < 0) {
-      return [
-        {
-          xL: xL,
-          fxL: fxL
-        },
-        {
-          xU: xR,
-          fxU: fxR
-        }
-      ]
-    }
-  }
+    
+  })
+  return integralFormatted;
 }
 
 module.exports = { calcular }
